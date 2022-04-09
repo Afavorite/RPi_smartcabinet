0fc3a415a6a8410c9065c3e68b0d782eb78b8c7c
AFavo <lai_kaiwen@qq.com>
2022-04-09 20:23:13 +0800

    优化显示

--- call_mainwindow.py
+++ call_mainwindow.py
@@ -4,15 +4,18 @@ from PyQt5.QtWidgets import *
 from ui.mainwindow import Ui_Form
 
 class MainPageWindow(QWidget,Ui_Form):
+    # MainPageWindow继承自mainwindow.Ui_Form，实现UI和逻辑之间的分离
     def __init__(self, parent=None):
         super(MainPageWindow, self).__init__(parent)
         self.setupUi(self)
         self.initUI()
 
     def initUI(self):
+        #
         self.timer = QTimer()
         self.timer.timeout.connect(self.showtime)  # 这个通过调用槽函数来刷新时间
         self.timer.start(1000)  # 每隔一秒刷新一次，这里设置为1000ms
+
     def showtime(self):
         time = QDateTime.currentDateTime()  # 获取当前时间
         timedisplay = time.toString("yyyy-MM-dd hh:mm:ss dddd")  # 格式化一下时间
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-import ui.window
 from call_mainwindow import MainPageWindow
 from PyQt5.QtWidgets import QApplication, QWidget
 
@@ -8,10 +7,6 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     m = MainPageWindow()
-    # 实例ui
-    # UI = ui.window.Ui_Form()
-    # 主窗口添加控件
-    # UI.setupUi(m)
 
     m.show()
     sys.exit(app.exec_())
