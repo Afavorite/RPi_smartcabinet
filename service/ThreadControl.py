ab2d226a36768039b0ddae2b458fb79df6b6278c
AFavo <lai_kaiwen@qq.com>
2022-04-27 22:47:56 +0800

    a

--- service/ThreadControl.py
+++ service/ThreadControl.py
@@ -33,9 +33,10 @@ class ThreadControl(QThread):
         GPIO.setwarnings(False)
         GPIO.setup(self.IN_ster, GPIO.OUT)
         GPIO.setup(self.IN_lock, GPIO.OUT)
-        GPIO.setup(self.IN_lock_feedback, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.IN_lock_feedback, GPIO.IN)# , pull_up_down=GPIO.PUD_DOWN)
         # 侦测到关门信号，进入中断函数
-        GPIO.add_event_detect(self.IN_lock_feedback, GPIO.RISING, callback=self.lock_callback, bouncetime=200)
+        # GPIO.add_event_detect(self.IN_lock_feedback, GPIO.RISING, callback=self.lock_callback, bouncetime=200)
+        GPIO.add_event_detect(self.IN_lock_feedback, GPIO.RISING)
 
     def run(self):
         # controlfb = {}
@@ -49,27 +50,37 @@ class ThreadControl(QThread):
                 self.controlfb['ster'] = 'off'
             time.sleep(0.75)
 
+            if GPIO.event_detected(self.IN_lock_feedback):
+                print('门锁已关闭')
+                self.flag = True
+                self.controlfb['lock'] = 'lock'
+                t = Thread(target=self.timewait)
+                t.start()
+
             if self.order_lock == 'unlock' and self.controlfb['lock'] == 'lock' and self.flag is False:
-                self.controlfb['lock'] = 'unlock'
-                print('门锁通电')
-                GPIO.output(self.IN_lock, True)
-                GPIO.remove_event_detect(self.IN_lock_feedback)
-                time.sleep(0.5)
-                print('门锁断电')
-                GPIO.output(self.IN_lock, False)
-                GPIO.add_event_detect(self.IN_lock_feedback, GPIO.RISING, callback=self.lock_callback, bouncetime=200)
+                sleep(0.5)
+                if self.order_lock == 'unlock' and self.controlfb['lock'] == 'lock' and self.flag is False:
+                    self.controlfb['lock'] = 'unlock'
+                    print('门锁通电')
+                    GPIO.output(self.IN_lock, True)
+                    # GPIO.remove_event_detect(self.IN_lock_feedback)
+                    time.sleep(1)
+                    print('门锁断电')
+                    GPIO.output(self.IN_lock, False)
+                    # time.sleep(1)
+                    # GPIO.add_event_detect(self.IN_lock_feedback, GPIO.RISING)
 
             self.SignalControl.emit(self.controlfb)
 
     # 防止门锁关闭后，由于指令的延时导致门锁再次打开
-    def lock_callback(self, IN_lock_feedback):
-        print('门锁已关闭')
-        self.flag = True
-        self.controlfb['lock'] = 'lock'
-        t = Thread(target=self.timewait)
-        t.start()
+    # def lock_callback(self, IN_lock_feedback):
+    #     print('门锁已关闭')
+    #     self.flag = True
+    #     self.controlfb['lock'] = 'lock'
+    #     t = Thread(target=self.timewait)
+    #     t.start()
 
     # 延时一段时间，保证服务器收到门锁已关闭的信号，并修改指令
-    def timewait(self):
-        sleep(3)
-        self.flag = False
+    # def timewait(self):
+    #     sleep(3)
+    #     self.flag = False
