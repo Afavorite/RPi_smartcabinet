35a6ba759409160c128ad4cf05a07460f1472510
AFavo <lai_kaiwen@qq.com>
2022-05-05 15:31:46 +0800

    添加温度控制线程，添加增量式PID控制

--- call_mainwindow.py
+++ call_mainwindow.py
@@ -3,7 +3,7 @@ from PyQt5.QtCore import QTimer, QDateTime
 from PyQt5.QtWidgets import *
 from ui.mainwindow import Ui_Form
 from service.ThreadHttp import ThreadHttp
-from service.ThreadControl import ThreadControl
+from service.ThreadControl import ThreadControl, ThreadTemp, IncrementalPID
 
 import temp_get
 
@@ -15,11 +15,14 @@ class MainPageWindow(QWidget, Ui_Form):
 
     def __init__(self, parent=None):
         super(MainPageWindow, self).__init__(parent)
+        self.thread_temp = ThreadTemp()
         self.thread_conn = ThreadHttp()
         self.thread_control = ThreadControl()
+        # self.thread_temp = ThreadTemp()
         self.timer_show = QTimer()
         self.setupUi(self)
         self.initUI()
+        self.tempflag = 0
 
     def initUI(self):
         # 紫外线灯和门锁控制线程
@@ -29,6 +32,7 @@ class MainPageWindow(QWidget, Ui_Form):
         self.thread_conn.SignalHttp.connect(self.receiveHttp)
         self.thread_conn.start()
         # 设置定时器，刷新显示
+        temp_get.setup()
         self.timer_show.timeout.connect(self.display)  # 这个通过调用槽函数来刷新显示
         self.timer_show.start(1000)  # 每隔一秒刷新一次，这里设置为1000ms
 
@@ -37,28 +41,48 @@ class MainPageWindow(QWidget, Ui_Form):
         timedisplay = time.toString("yyyy-MM-dd hh:mm:ss")  # 格式化一下时间
         self.label_displaytime.setText(timedisplay)
 
-        temp_get.setup()
         temperature = temp_get.read()
         self.label_displaytemp.setText('当前温度：' + str(round(temperature, 2)) + '℃')
 
     def receiveHttp(self, r_dict):
         if r_dict['control_flag'] == 'booking':
-            self.thread_control.order_temp = 'stop'
+            self.label_displaytempswitch.setText('温控已关闭')
+            if self.thread_temp.isRunning():
+                self.tempflag = 0
+                self.thread_temp.canrun = False
+                print('温控关闭')
             self.thread_control.order_ster = 'off'
             self.thread_control.order_lock = 'lock'
             self.label_displaystatus.setText('箱柜已被预约')
+
         if r_dict['control_flag'] == 'using':
-            self.thread_control.order_temp = r_dict['control_temp']
+            if r_dict['control_temp'] != 'stop' and self.tempflag == 0:
+                self.tempflag = 1
+                self.thread_temp.canrun = True
+                self.thread_temp.start()
+                self.label_displaytempswitch.setText('温控已打开')
+
             self.thread_control.order_ster = r_dict['control_ster']
             self.thread_control.order_lock = 'lock'
             self.label_displaystatus.setText('箱柜使用中')
+
         if r_dict['control_flag'] == 'unlock':
-            self.thread_control.order_temp = 'stop'
+            self.label_displaytempswitch.setText('温控已关闭')
+            if self.thread_temp.isRunning():
+                self.tempflag = 0
+                self.thread_temp.canrun = False
+                print('温控关闭')
             self.thread_control.order_ster = 'off'
             self.thread_control.order_lock = 'unlock'
             self.label_displaystatus.setText('箱柜已解锁')
+
         if r_dict['control_flag'] == 'finish':
-            self.thread_control.order_temp = 'stop'
+            # self.thread_control.order_temp = 'stop'
+            self.label_displaytempswitch.setText('温控已关闭')
+            if self.thread_temp.isRunning():
+                self.tempflag = 0
+                self.thread_temp.canrun = False
+                print('温控关闭')
             self.thread_control.order_ster = 'off'
             self.thread_control.order_lock = 'lock'
             self.label_displaystatus.setText('箱柜空闲')
--- service/ThreadControl.py
+++ service/ThreadControl.py
@@ -104,3 +104,57 @@ class ThreadControl(QThread):
         sleep(5)
         self.flag = False
 
+
+class ThreadTemp(QThread):
+    canrun = True
+    # temp_get.setup()
+
+    def __init__(self):
+        super().__init__()
+        print('温控初始化')
+
+    def run(self):
+        inc_pid = IncrementalPID(5.0, 0.2, 0.1)
+        while True:
+            inc_pid.SetStepSignal(30.0)
+            print(inc_pid.PIDOutput)
+            sleep(0.5)
+            if self.canrun is False:
+                return
+
+
+class IncrementalPID:
+    def __init__(self, P: float, I: float, D: float):
+        self.Kp = P
+        self.Ki = I
+        self.Kd = D
+
+        self.PIDOutput = 0.0  # PID控制器输出
+        self.SystemOutput = 0.0  # 系统输出值
+        self.LastSystemOutput = 0.0  # 系统的上一次输出
+
+        self.Error = 0.0
+        self.LastError = 0.0
+        self.LastLastError = 0.0
+
+        temp_get.setup()
+
+    # 设置PID控制器参数
+    def SetStepSignal(self, StepSignal):
+        # self.Error = StepSignal - self.SystemOutput
+        self.Error = StepSignal - temp_get.read()
+        # 计算增量
+        IncrementalValue = self.Kp * (self.Error - self.LastError) \
+                           + self.Ki * self.Error + self.Kd * (self.Error - 2 * self.LastError + self.LastLastError)
+        # 计算输出
+        self.PIDOutput += IncrementalValue
+        print(self.PIDOutput)
+        self.LastLastError = self.LastError
+        self.LastError = self.Error
+
+    # 以一阶惯性环节为例子演示控制效果
+    # def SetInertiaTime(self, IntertiaTime, SampleTime):
+    #     self.SystemOutput = (IntertiaTime * self.LastSystemOutput + SampleTime * self.PIDOutput) / (
+    #                 SampleTime + IntertiaTime)
+    #     self.LastSystemOutput = self.SystemOutput
+
